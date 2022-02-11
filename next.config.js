1a1a65df65ca43d482f921f36a60c44661a2e338
cedoor <me@cedoor.dev>
2022-02-11 14:53:06 +0100

    build: add webpack conf for snarkjs node pkgs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        if (!isServer) {
+            config.plugins.push(
+                new webpack.ProvidePlugin({
+                    global: "global"
+                })
+            )
+
+            config.resolve.fallback = {
+                fs: false,
+                stream: false,
+                crypto: false,
+                os: false,
+                readline: false,
+                ejs: false,
+                assert: require.resolve("assert"),
+                path: false
+            }
+
+            return config
+        }
+
+        return config
+    }
 }
 
 module.exports = nextConfig
