e22a6dad39c24b7f19ef93b5e7db19541ce980c2
cedoor <me@cedoor.dev>
2022-06-27 19:25:41 +0200

    feat: add cors and api to add members

--- apps/relay/index.ts
+++ apps/relay/index.ts
@@ -1,39 +1,50 @@
-import { config as dotenvConfig } from "dotenv"
-import { abi as contractAbi } from "../contracts/build/contracts/contracts/Reviews.sol/Reviews.json"
+import cors from "cors"
 import { Contract, providers, utils, Wallet } from "ethers"
 import express from "express"
-import { resolve } from "path"
+import { abi as contractAbi } from "../contracts/build/contracts/contracts/Events.sol/Events.json"
 
-dotenvConfig({ path: resolve(__dirname, "./.env") })
-
-if (typeof process.env.CONTRACT_ADDRESS !== "string") {
-    throw new Error("Please, define CONTRACT_ADDRESS in your .env file")
-}
-
-if (typeof process.env.ETHEREUM_JSON_PROVIDER !== "string") {
-    throw new Error("Please, define ETHEREUM_JSON_PROVIDER in your .env file")
-}
-
-if (typeof process.env.ETHEREUM_PRIVATE_KEY !== "string") {
-    throw new Error("Please, define ETHEREUM_PRIVATE_KEY in your .env file")
-}
-
-const ethereumPrivateKey = process.env.ETHEREUM_PRIVATE_KEY
-const ethereumJsonProvider = process.env.ETHEREUM_JSON_PROVIDER
-const contractAddress = process.env.CONTRACT_ADDRESS
-const port = Number(process.env.API_PORT) || 3000
+const ethereumPrivateKey = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+const ethereumJsonProvider = "http://localhost:8545"
+const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+const port = 3000
 
 const app = express()
 
-app.get("/", async (req, res) => {
-    const { review, nullifierHash, groupId, solidityProof } = JSON.parse(req.body)
+app.use(cors())
+app.use(express.json())
 
-    const provider = new providers.JsonRpcProvider(ethereumJsonProvider)
-    const signer = new Wallet(ethereumPrivateKey, provider)
-    const contract = new Contract(contractAddress, contractAbi, signer)
+const provider = new providers.JsonRpcProvider(ethereumJsonProvider)
+const signer = new Wallet(ethereumPrivateKey, provider)
+const contract = new Contract(contractAddress, contractAbi, signer)
+
+app.post("/post-review", async (req, res) => {
+    const { review, nullifierHash, groupId, solidityProof } = req.body
 
     try {
-        await contract.postReview(utils.formatBytes32String(review), nullifierHash, groupId, solidityProof)
+        const transaction = await contract.postReview(
+            utils.formatBytes32String(review),
+            nullifierHash,
+            groupId,
+            solidityProof
+        )
+
+        await transaction.wait()
+
+        res.status(200).end()
+    } catch (error: any) {
+        console.error(error)
+
+        res.status(500).end()
+    }
+})
+
+app.post("/add-member", async (req, res) => {
+    const { groupId, identityCommitment } = req.body
+
+    try {
+        const transaction = await contract.addMember(groupId, identityCommitment)
+
+        await transaction.wait()
 
         res.status(200).end()
     } catch (error: any) {
