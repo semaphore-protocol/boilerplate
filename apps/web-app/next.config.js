3f5225968afeead0d608c5241b2fdf8b63aaee71
cedoor <me@cedoor.dev>
2022-11-12 18:13:55 +0100

    fix: remove unused ethereum url env variable

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     publicRuntimeConfig: {
-        ETHEREUM_URL: process.env.ETHEREUM_URL,
         ETHEREUM_CHAIN_ID: process.env.ETHEREUM_CHAIN_ID,
         CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS
     },
