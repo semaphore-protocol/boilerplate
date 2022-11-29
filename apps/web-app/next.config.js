bb489b46a36163acdc05909dd1c848e7315dc9c9
cedoor <me@cedoor.dev>
2022-11-29 12:07:48 +0100

    chore: add env variables to next conf

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const dotenv = require("dotenv")
+
+dotenv.config({ path: "../../.env" })
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    env: {
+        ETHEREUM_PRIVATE_KEY: process.env.ETHEREUM_PRIVATE_KEY,
+        ETHEREUM_URL: process.env.ETHEREUM_URL,
+        CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS
+    },
     publicRuntimeConfig: {
         ETHEREUM_CHAIN_ID: process.env.ETHEREUM_CHAIN_ID,
         CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS
