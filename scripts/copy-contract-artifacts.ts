627bcade97d3e02007c3e98db854f0e45c2a5d4b
Vivian Plasencia <v.pcalana@gmail.com>
2023-01-20 18:46:21 -0500

    Update scripts/copy-contract-artifacts.ts
    Co-authored-by: cedoor <me@cedoor.dev>

--- scripts/copy-contract-artifacts.ts
+++ scripts/copy-contract-artifacts.ts
@@ -6,7 +6,21 @@ const subgraphArtifactsPath = "apps/subgraph/contract-artifacts"
 
 const webAppArtifactsPath = "apps/web-app/contract-artifacts"
 
-async function copyArtifactsFiles() {
+async function main() {
+   try {
+        await fs.promises.copyFile(`${contractArtifactsPath}/Feedback.json`, `${subgraphArtifactsPath}/Feedback.json`)
+        await fs.promises.copyFile(`${contractArtifactsPath}/Feedback.json`, `${webAppArtifactsPath}/Feedback.json`)
+    } catch (err) {
+        throw new Error("Error occurred while copying the contract artifacts files")
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
     try {
         await fs.promises.copyFile(`${contractArtifactsPath}/Feedback.json`, `${subgraphArtifactsPath}/Feedback.json`)
         await fs.promises.copyFile(`${contractArtifactsPath}/Feedback.json`, `${webAppArtifactsPath}/Feedback.json`)
