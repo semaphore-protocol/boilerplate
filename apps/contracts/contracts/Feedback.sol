cac0ab6f7f2657652c90a0941123bc139e198fbc
cedoor <me@cedoor.dev>
2023-03-15 21:27:44 +0000

    refactor(contracts): remove usernames and events

--- apps/contracts/contracts/Feedback.sol
+++ apps/contracts/contracts/Feedback.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.4;
 import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
 
 contract Feedback {
-    error Feedback__UsernameAlreadyExists();
-
-    event NewFeedback(bytes32 feedback);
-    event NewUser(uint256 identityCommitment, bytes32 username);
-
     ISemaphore public semaphore;
 
     uint256 public groupId;
-    mapping(uint256 => bytes32) public users;
 
     constructor(address semaphoreAddress, uint256 _groupId) {
         semaphore = ISemaphore(semaphoreAddress);
@@ -21,26 +15,16 @@ contract Feedback {
         semaphore.createGroup(groupId, 20, address(this));
     }
 
-    function joinGroup(uint256 identityCommitment, bytes32 username) external {
-        if (users[identityCommitment] != 0) {
-            revert Feedback__UsernameAlreadyExists();
-        }
-
+    function joinGroup(uint256 identityCommitment) external {
         semaphore.addMember(groupId, identityCommitment);
-
-        users[identityCommitment] = username;
-
-        emit NewUser(identityCommitment, username);
     }
 
     function sendFeedback(
-        bytes32 feedback,
+        uint256 feedback,
         uint256 merkleTreeRoot,
         uint256 nullifierHash,
         uint256[8] calldata proof
     ) external {
-        semaphore.verifyProof(groupId, merkleTreeRoot, uint256(feedback), nullifierHash, groupId, proof);
-
-        emit NewFeedback(feedback);
+        semaphore.verifyProof(groupId, merkleTreeRoot, feedback, nullifierHash, groupId, proof);
     }
 }
