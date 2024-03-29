d7c47b11735a4b0d8e6675ffdb272c015f8f1fd8
Varun Sharma <sharmav4568@gmail.com>
2024-03-29 23:52:22 +0530

    reversing groups and pages list to make recent ones appear first

--- apps/web-app/src/app/group/page.tsx
+++ apps/web-app/src/app/group/page.tsx
@@ -133,7 +133,6 @@ export default function GroupsPage() {
             {_users.length > 0 && (
                 <VStack spacing="3" pb="3" align="left" maxHeight="300px" overflowY="scroll">
                     {users.map((user, i) =>(
-                      
                         <HStack key={i} pb="3" borderBottomWidth={i < _users.length - 1 ? 1 : 0} whiteSpace="nowrap">
                             <Text textOverflow="ellipsis" overflow="hidden">
                                 {_identity?.commitment === user ? <b>{user}</b> : user}
